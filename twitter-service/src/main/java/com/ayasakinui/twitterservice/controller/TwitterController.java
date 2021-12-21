f7e48dd3f787dce7e334a83c73c8186025276d99
shichikoromo <nanae.sai@mailbox.org>
2021-12-21 11:18:45 +0100

    Update Twitter OAth

--- twitter-service/src/main/java/com/ayasakinui/twitterservice/controller/TwitterController.java
+++ twitter-service/src/main/java/com/ayasakinui/twitterservice/controller/TwitterController.java
@@ -3,12 +3,18 @@ package com.ayasakinui.twitterservice.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 
 import javax.annotation.PostConstruct;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/twitter")
@@ -24,20 +30,40 @@ public class TwitterController {
     }
 
     @RequestMapping("user")
-    public String auth() throws Exception{
+    public String auth() throws Exception {
 
         String out =
                 "<b>twitter: </b><br>" + String.valueOf(twitter) + "<br><br>" +
-                "<b>twitter.getOAuthRequestToken(): </b><br>" + String.valueOf(twitter.getOAuthRequestToken());// +
-                //"<b>twitter.getOAuthAccessToken(): </b><br>" + String.valueOf(twitter.getOAuthAccessToken());
+                        "<b>twitter.getOAuthRequestToken(): </b><br>" + String.valueOf(twitter.getOAuthRequestToken());// +
+        //"<b>twitter.getOAuthAccessToken(): </b><br>" + String.valueOf(twitter.getOAuthAccessToken());
 
         return out;
     }
 
-    @RequestMapping("test")
-    public String test() throws Exception{
-        RequestToken requestToken = twitter.getOAuthRequestToken();
-        return requestToken.getAuthorizationURL();
+    @RequestMapping("success")
+    public String success() {
+        return "success!!";
+    }
+
+    @RequestMapping(value = "test",method=RequestMethod.GET)
+    public String test() throws Exception {
+        String callbackURL = "http://localhost:8080/twitter/success";
+        RequestToken requestToken = twitter.getOAuthRequestToken(callbackURL);
+
+        AccessToken accessToken = null;
+        String url = requestToken.getAuthorizationURL();
+        System.out.println(requestToken.getAuthorizationURL());
+        while (null == accessToken) {
+            try {
+                Desktop.getDesktop().browse(new URI(requestToken.getAuthorizationURL()));
+            } catch (UnsupportedOperationException ignore) {
+            } catch (IOException ignore) {
+            } catch (URISyntaxException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        return "redirect:"+url;
     }
     /*
     public User user() throws Exception {
