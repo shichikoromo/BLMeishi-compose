3f91da549e651ae93f0809eb3a8a31ebed76390c
shichikoromo <nanae.sai@mailbox.org>
2022-01-10 14:13:52 +0100

    Update Twitter OAth

--- twitter-service/src/main/java/com/ayasakinui/twitterservice/controller/TwitterController.java
+++ twitter-service/src/main/java/com/ayasakinui/twitterservice/controller/TwitterController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 import javax.annotation.PostConstruct;
 
@@ -18,8 +21,15 @@ import javax.annotation.PostConstruct;
 public class TwitterController {
 
     private static final Logger log = LoggerFactory.getLogger(TwitterController.class);
+    private static final String CONSUMER_KEY = "consumer key";
+    private static final String CONSUMER_SECRET = "consumer secret";
+
+    private static String ACCES_TOKEN = null;
+    private static String ACCES_SECRET = null;
+
 
     private Twitter twitter;
+    private RequestToken requestToken;
 
     @PostConstruct
     private void ctor() {
@@ -31,7 +41,7 @@ public class TwitterController {
 
         String out =
                 "<b>twitter: </b><br>" + String.valueOf(twitter) + "<br><br>" +
-                        "<b>twitter.getOAuthRequestToken(): </b><br>" + String.valueOf(twitter.getOAuthRequestToken());// +
+                "<b>twitter.getOAuthRequestToken(): </b><br>" + String.valueOf(twitter.getOAuthRequestToken());// +
         //"<b>twitter.getOAuthAccessToken(): </b><br>" + String.valueOf(twitter.getOAuthAccessToken());
 
         return out;
@@ -40,20 +50,55 @@ public class TwitterController {
     @RequestMapping(value = "oauth")
     public RedirectView oauth() throws Exception {
         String callbackURL = "http://localhost:8080/twitter/success";
-        RequestToken requestToken = twitter.getOAuthRequestToken(callbackURL);
-        return new RedirectView(requestToken.getAuthorizationURL());
+        requestToken = twitter.getOAuthRequestToken(callbackURL);
+
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setOAuthConsumerKey(CONSUMER_KEY);
+        configurationBuilder.setOAuthConsumerSecret(CONSUMER_SECRET);
+        Configuration configuration = configurationBuilder.build();
+        twitter = new TwitterFactory(configuration).getInstance();
+
+        return new RedirectView(requestToken.getAuthenticationURL());
     }
 
     @RequestMapping("success")
-    public String success(@RequestParam String oauth_token, @RequestParam String oauth_verifier) throws Exception{
-        twitter.setOAuthAccessToken(new AccessToken(oauth_token,oauth_verifier));
+    public String success(@RequestParam String oauth_token, @RequestParam String oauth_verifier) throws Exception {
 
-        String success = "success!!<br><br>"+
-                "<b>twitter: </b><br>" + String.valueOf(twitter) + "<br><br>" +
-                "<b>twitter: </b><br>" + String.valueOf(twitter) + "<br><br>" +
-                "<b>oauth_token:</b>  " + oauth_token +  "<br><br>" +
-                "<b>oauth_verifier: </b> " + oauth_verifier + "<br><br>"; // +
-                //"<b>Screen Name: </b> " +twitter.verifyCredentials().getScreenName();
+        //アクセストークンを取得
+        AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, oauth_verifier);
+
+        ACCES_TOKEN = accessToken.getToken();
+        ACCES_SECRET = accessToken.getTokenSecret();
+
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setApplicationOnlyAuthEnabled(false);
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(CONSUMER_KEY)
+                .setOAuthConsumerSecret(CONSUMER_SECRET)
+                .setOAuthAccessToken(ACCES_TOKEN)
+                .setOAuthAccessTokenSecret(ACCES_SECRET);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter = tf.getInstance();
+
+
+        User user = twitter.verifyCredentials();
+
+
+        String success = "success!!<br><br>" +
+                "<b>twitter: </b><br>" + twitter + "<br><br>" +
+                //"<b>TwitterFactory: </b><br>" + String.valueOf(tf) + "<br><br>" +
+                "<b>accesToken: </b><br>" + accessToken + "<br><br>" +
+                "<b>accesToken.getToken(): </b><br>" + accessToken.getToken() + "<br><br>" +
+                "<b>accesToken.getTokenSecret(): </b><br>" + accessToken.getTokenSecret() + "<br><br>" +
+                "<b>user: </b><br>" + user + "<br><br>" +
+                "<b>user id: </b>" + user.getName() + "<br><br>" +
+                "<b>user name: </b>" + user.getScreenName() + "<br><br>" +
+                "<b>user icon: </b><img src=\"" + user.get400x400ProfileImageURL() + "\"><br><br>" +
+                "<b>user follower: </b>" + user.getFollowersCount() + "<br><br>" +
+                "<b>user fav: </b>" + user.getFavouritesCount() + "<br><br>" +
+                "<b>oauth_token:</b>  " + oauth_token + "<br><br>" +
+                "<b>oauth_verifier: </b> " + oauth_verifier + "<br><br>";
+        //"<b>Screen Name: </b> " + twitter.getScreenName();
         return success;
     }
 
