0e34de7c14bf12ccaa1a2cedc123b740c0d0a6cc
AyakaHashimoto <ayaka.seven.11@gmail.com>
2022-02-17 21:35:31 +0900

    fixed static referencce error

--- twitter-service/src/main/java/com/ayasakinui/twitterservice/view/MainController.java
+++ twitter-service/src/main/java/com/ayasakinui/twitterservice/view/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Optional;
 
 import com.ayasakinui.twitterservice.dataAccess.entity.Member;
 import com.ayasakinui.twitterservice.service.memberdomService;
@@ -16,7 +17,9 @@ public class MainController {
 
     @RequestMapping(value = "dom/{domId}")//各domのページ
     public String domPage(Model model, @PathVariable("domId") Long domId) {
-        List<Member> memberList=memberdomService.getMembers(domId);
+        memberdomService memberdomService = new memberdomService();
+        List<Long> memberIds = memberdomService.findMemberIds(domId);
+        List<Optional> memberList = memberdomService.findMembers(memberIds);
         model.addAttribute("memberList", memberList);
         return "hello";
     }
